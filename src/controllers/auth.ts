6aa956f1f09d3b623dd33a5d8a3ac00e76c3c833
Irabeny <irabeny89@gmail.com>
2024-10-02 23:20:23 +0100

    style(auth): format code

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,6 +1,18 @@
 import { Elysia, t } from "elysia";
-import { type TokenT, clearAuthTokens, setAuthTokens, verifyToken } from "../lib/token-mgr";
-import { User, cookieSchema, loginSchema, logoutSchema, refreshSchema, signupSchema } from "../models/user";
+import {
+	type TokenT,
+	clearAuthTokens,
+	setAuthTokens,
+	verifyToken,
+} from "../lib/token-mgr";
+import {
+	User,
+	cookieSchema,
+	loginSchema,
+	logoutSchema,
+	refreshSchema,
+	signupSchema,
+} from "../models/user";
 import type { ResponseT } from "../types";
 
 const auth = new Elysia({ name: "auth" })
@@ -19,29 +31,30 @@ const auth = new Elysia({ name: "auth" })
 			// mobile app - no access to cookie i.e `body.refresh`
 			// refresh token probably stored somewhere else
 			// while web app - access to cookie i.e `cookie.refresh.value`
-			let refresh: string | undefined = body.refresh || cookie.refresh.value
+			const refresh: string | undefined = body.refresh || cookie.refresh.value;
 
-			if (!refresh) return error<401, ResponseT>(401, {
-				success: false,
-				message: "Unauthorized. Go and signup/login."
-			})
+			if (!refresh)
+				return error<401, ResponseT>(401, {
+					success: false,
+					message: "Unauthorized. Go and signup/login.",
+				});
 
 			// * refresh token is id (literal string)
-			const userId = verifyToken(refresh) as string
-			const user = User.findById(+userId)
+			const userId = verifyToken(refresh) as string;
+			const user = User.findById(+userId);
 
 			return user
 				? {
-					message: "Token refresh successful.",
-					success: true,
-					data: setAuthTokens(cookie, user),
-				}
+						message: "Token refresh successful.",
+						success: true,
+						data: setAuthTokens(cookie, user),
+					}
 				: error<404, ResponseT>(404, {
-					success: false,
-					message: "User not found."
-				})
+						success: false,
+						message: "User not found.",
+					});
 		},
-		{ cookie: "cookie", tags: ["Auth"], body: "refresh" }
+		{ cookie: "cookie", tags: ["Auth"], body: "refresh" },
 	)
 	.post(
 		"/logout",
@@ -77,8 +90,8 @@ const auth = new Elysia({ name: "auth" })
 					const user = User.findBy("email", body.email) as User;
 
 					const res: ResponseT<TokenT> = {
-						message: "User created",
 						success: true,
+						message: "User created",
 						data: setAuthTokens(cookie, user),
 					};
 
