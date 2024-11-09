2d2966df93794984c971e078049abfa38dcc947f
Irabeny <irabeny89@gmail.com>
2024-11-09 12:10:48 +0100

    chore(dashboard): add click event to page numbers on table

--- public/js/table.js
+++ public/js/table.js
@@ -3,6 +3,14 @@ const currPage = searchParams.get("page");
 
 const prevBtn = document.getElementById("previous-button");
 const nextBtn = document.getElementById("next-button");
+const pagingNumbers = document.querySelectorAll("[id^=table_pager_]");
+// add click event to navigate on every page numbers on pagination
+pagingNumbers.forEach((el) => {
+  el.addEventListener("click", () => {
+    searchParams.set("page", el.textContent);
+    window.location.search = searchParams;
+  });
+});
 
 prevBtn.onclick = () => {
   if (currPage > 1) {
