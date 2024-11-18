33433ccddfc16b869c7a169b44bd2aff8dd90624
Irabeny <irabeny89@gmail.com>
2024-11-18 03:46:16 +0100

    feat(dashboard ui): add update delivery fee

--- public/js/info.js
+++ public/js/info.js
@@ -3,9 +3,14 @@ import { apiClient } from "./utils.js";
 const gasPriceBtn = document.getElementById("gas-price-btn");
 const deliveryFeeBtn = document.getElementById("delivery-fee-btn");
 const gasPriceDialog = document.getElementById("gas-price-dialog");
+const deliveryFeeDialog = document.getElementById("delivery-fee-dialog");
 const gasPriceForm = document.getElementById("gas-price-form");
+const deliveryFeeForm = document.getElementById("delivery-fee-form");
 const gasPriceDialogCloseBtn = document.getElementById(
   "gas-price-dialog-close-btn",
+);
+const deliveryFeeDialogCloseBtn = document.getElementById(
+  "delivery-fee-dialog-close-btn",
 );
 
 gasPriceDialogCloseBtn.onclick = () => {
@@ -20,6 +25,22 @@ gasPriceForm.onsubmit = async (e) => {
   e.preventDefault();
   const pricePerKg = Number(new FormData(gasPriceForm).get("pricePerKg"));
   const res = await apiClient.setting.update({ pricePerKg });
+  if (res.ok) window.location.reload();
+  else alert("Something went wrong. Try again or contact support.");
+};
+
+deliveryFeeDialogCloseBtn.onclick = () => {
+  deliveryFeeDialog.close();
+};
+
+deliveryFeeBtn.onclick = () => {
+  deliveryFeeDialog.showModal();
+};
+
+deliveryFeeForm.onsubmit = async (e) => {
+  e.preventDefault();
+  const deliveryFee = Number(new FormData(deliveryFeeForm).get("deliveryFee"));
+  const res = await apiClient.setting.update({ deliveryFee });
   if (res.ok) window.location.reload();
   else alert("Something went wrong. Try again or contact support.");
 };
